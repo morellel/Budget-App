d7765223d0c02b7a4066500cebf7861111344b03
Emma Morell <emma.morell8888@gmail.com>
2023-08-14 17:07:29 -0400

    build works

---
+++ budget-spring-boot/src/main/java/com/budget/budgetspringboot/BudgetSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.budget.budgetspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetSpringBootApplication.class, args);
+	}
+
+}
---
+++ budget-spring-boot/src/test/java/com/budget/budgetspringboot/BudgetSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budget.budgetspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
